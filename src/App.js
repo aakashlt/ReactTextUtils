3b5f8f8bfd81b888fce11af051eff33556f34383
aakashlt <Taloleaakash@gmail.com>
2021-10-16 23:53:14 +0530

    Commit text utility
    Commit text utility

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+     if(mode==="light"){
+       setMode("dark");
+       document.body.style.backgroundColor= "grey"
+     }
+     else{
+       setMode("light");
+       document.body.style.backgroundColor= "white"
+     }
+  }
+  
   return (
     <>
         
       
-        <NavBar title="Textutils" aboutText="About Us"/>
+        <NavBar title="Textutils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
         <TextForm heading="Enter Text here"/>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,29 +4,69 @@ export default function TextForm(props) {
     
   //  text = "Enter Any text here"; Wrong way to set the text
    // setText("set text");
-   const handleUpClick = ()=>{
+  //buttom hendled
+   const handleUpperCaseClick = ()=>{
     let UpperCaseValue = text.toUpperCase();
     setText(UpperCaseValue);
-       console.log("You have clicked on text");
+       console.log("You have clicked on handleUpperCaseClick");
    }
+   
+   const handleLowerCaseClick = ()=>{
+    let lowweCaseValue = text.toLowerCase();
+    setText(lowweCaseValue);
+       console.log("You have clicked on handleLowerCaseClick");
+   }
+   const handleCopyTextClick = ()=>{
+      let copyText = document.getElementById("textFeild");
+      /* Select the text field */
+      copyText.select();
+       /* Copy the text inside the text field */
+      navigator.clipboard.writeText(copyText.value);
+    
+   }
+   const ClearSpace = ()=>{
+   
+    let SpaceText  = text.split(/[ ]+/);
+    console.log(SpaceText);
+    let newString  = SpaceText.join(" ")
+    setText(newString);
+    
+   }
+
+
+   // on change eventn for text
    const handleOnChange = (event)=>{
-      
     setText(event.target.value);
     console.log("Click On OnChange",event.target.value);
     }
 
-const [text, setText] = useState('Enter Text Here');
+
+//use react hook
+
+const [text, setText] = useState('');
+
+
+
   return (
     <div className="container">
       <h1>{props.heading}</h1>
       <textarea
-        className="form-control" onChange={handleOnChange}
+        className="form-control" onChange={handleOnChange} id="textFeild"
        value={text}
         rows="8"
       ></textarea>
       <div className="col-auto">
-        <button type="submit" onClick={handleUpClick} className="btn btn-primary mb-3">
-          Enter 
+        <button  type="submit" onClick={handleUpperCaseClick} className="btn btn-primary mb-3 my-3">
+          UPPERCASE
+        </button>
+        <button type="submit" onClick={handleLowerCaseClick} className="btn btn-primary mb-3 my-3">
+          lowercase
+        </button>
+        <button type="submit" onClick={handleCopyTextClick} className="btn btn-primary mb-3 my-3" >
+          copy text
+        </button>
+        <button type="submit" onClick={ClearSpace} className="btn btn-primary mb-3 my-3 ">
+          clear space
         </button>
       </div>
     </div>
